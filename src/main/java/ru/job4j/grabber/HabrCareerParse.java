90590ea2ec69e0616acc5d1e1411ee4292494869
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-08-19 00:39:06 +0300

    Парсинг https://career.habr.com/vacancies/java_developer?page=1

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -8,6 +8,8 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HabrCareerParse {
 
@@ -20,38 +22,20 @@ public class HabrCareerParse {
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
     public static void main(String[] args) throws IOException {
-        /*Сначала мы получаем страницу, чтобы с ней можно было работать:
-        */
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-
-        /* Сначала мы получаем все вакансии страницы.
-        Перед CSS классом ставится точка. Это правила CSS селекторов, с которыми работает метод JSOUP select()
-        */
-
-        Elements rows = document.select(".vacancy-card__inner");
-        /*Проходимся по каждой вакансии и извлекаем нужные для нас данные.
-        Сначала получаем элементы содержащие название и ссылку.
-        Стоит обратить внимание, что дочерние элементы можно получать через индекс - метод child(0)
-        или же через селектор - select(".vacancy-card__title").
-         */
-        rows.forEach(row -> {
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            Element cardDate = row.select(".vacancy-card__date").first();
-            Element linkDateTime = cardDate.child(0);
-            /*
-            Наконец получаем данные непосредственно.
-            text() возвращает все содержимое элемента в виде текста,
-            т.е. весь текст что находится вне тегов HTML.
-            Ссылку находится в виде атрибута, поэтому ее значение надо получить как значение атрибута.
-            Для этого служит метод attr()
-             */
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            String dateTime = String.format(linkDateTime.attr("datetime"));
-            String[] dF = dateTime.split("T");
-            System.out.printf("%s %s %s%n", dF[0], vacancyName, link);
-        });
+        for (int page = 1; page < 3; page++) {
+            Connection connection = Jsoup.connect(PAGE_LINK + "?page=" + page);
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element linkElement = titleElement.child(0);
+                Element linkDateTime = row.select(".vacancy-card__date").first().child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                String dateTime = String.format(linkDateTime.attr("datetime"));
+                String[] dF = dateTime.split("T");
+                System.out.printf("%s %s %s%n", dF[0], vacancyName, link);
+            });
+        }
     }
 }

8349880c9c8d74b7b83041d232ccf1f1f2348202
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-08-19 00:28:14 +0300

    Преобразование даты

---
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -0,0 +1,14 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+public class HabrCareerDateTimeParser implements DateTimeParser {
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        return ZonedDateTime.parse(parse).toLocalDateTime();
+    }
+}

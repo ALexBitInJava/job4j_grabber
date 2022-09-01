9aa35fc00d96c5dfee243183111176da070da487
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-09-01 15:49:32 +0300

    Эксперименты с различными GC

--- src/main/java/ru/job4j/gc/prof/Sorting.java
+++ src/main/java/ru/job4j/gc/prof/Sorting.java
@@ -4,41 +4,45 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Sorting {
-    private int number;
-    private RandomArray randomArray;
+    private RandomArray array;
     private static String ln = System.lineSeparator();
-    private static String menu =
-             "1 - Сортировка пузырьком;" + ln
-            + "2 - Сортировка вставками;" + ln
-            + "3 - Сортировка слиянием;" + ln
-            + "4 - Выход.";
+    private static String menu = "1 - Создание массива" + ln
+            + "2 - Сортировка пузырьком;" + ln
+            + "3 - Сортировка вставками;" + ln
+            + "4 - Сортировка слиянием;" + ln
+            + "5 - Выход.";
 
     public static void main(String[] args) {
         Sorting sorting = new Sorting();
-        System.out.println("---Автоматическое создание массива---");
-        RandomArray array = new RandomArray(new Random());
-        array.insert(250000);
-        sorting.randomArray = array;
+        Scanner scanner = new Scanner(System.in);
         System.out.println("---Сортировка---");
-            System.out.println(menu);
-        System.out.println("---Введите цифру от 1 до 4---");
-            Scanner scanner = new Scanner(System.in);
-            sorting.number = Integer.parseInt(scanner.nextLine());
-            if (sorting.number == 1) {
-                BubbleSort bubbleSort = new BubbleSort();
-                bubbleSort.sort(sorting.randomArray);
-            }
-            if (sorting.number == 2) {
-                InsertSort insertSort = new InsertSort();
-                insertSort.sort(sorting.randomArray);
-            }
-            if (sorting.number == 3) {
-                MergeSort mergeSort = new MergeSort();
-                mergeSort.sort(sorting.randomArray);
-            }
-            if (sorting.number == 4) {
-            System.out.println("Выход из меню");
+        System.out.println(menu);
+        boolean rsl = true;
+        while (rsl) {
+            System.out.println("---Введите цифру от 1 до 5---");
 
+            int number = Integer.parseInt(scanner.nextLine());
+                if (number == 1) {
+                    RandomArray randomArray = new RandomArray(new Random());
+                    randomArray.insert(250000);
+                    sorting.array = randomArray;
+                }
+                if (number == 2) {
+                    BubbleSort bubbleSort = new BubbleSort();
+                    bubbleSort.sort(sorting.array);
+                }
+                if (number == 3) {
+                    InsertSort insertSort = new InsertSort();
+                    insertSort.sort(sorting.array);
+                }
+                if (number == 4) {
+                    MergeSort mergeSort = new MergeSort();
+                    mergeSort.sort(sorting.array);
+                }
+                if (number < 0 || number >= 5) {
+                    System.out.println("Выход из меню");
+                    rsl = false;
+                }
         }
     }
 }
